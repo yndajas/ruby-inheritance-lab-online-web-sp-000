eeb47093f3a3fd489809db36eaad5f4d2bcaed07
Ynda Jas <yndajas@gmail.com>
2020-07-23 14:19:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,15 @@
 require_relative "./user.rb"
 
 class Student  < User
+  
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(item)
+    @knowlede << item
+  end
+  
+  def knowledge
 
 end
